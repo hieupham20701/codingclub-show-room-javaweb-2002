40e63c322d33e9d02b55671edc3ec7f2ce17d64b
hieupham20701 <19519511.hieu@student.iuh.edu.vn>
2022-04-11 22:52:10 +0700

    mapping POJO class & fix applications.properties

--- src/main/java/com/se/showroomcar2002/entity/Brand.java
+++ src/main/java/com/se/showroomcar2002/entity/Brand.java
@@ -1,10 +1,13 @@
 package com.se.showroomcar2002.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -28,6 +31,9 @@ public class Brand {
 	@Column(name = "description")
 	private String description;
 
+	
+	@OneToMany(mappedBy = "brand")
+	private List<Product> products;
 	public Brand() {
 	}
 
